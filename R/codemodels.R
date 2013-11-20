4f0c2ee11c593186c791ecfadacc897ef8f591db
Ignacio Alvarez <ialvarez@iastate.edu>
2013-11-19 23:11:26 -0600

    add more models and rearrange code

---
+++ R/codemodels.R
@@ -0,0 +1,303 @@
+
+# Code with all the models considered, there are 6 cells: 
+#  Sigma:  Diff  Hier  Same
+#  Beta :  Diff  Hier 
+
+#--------------------------------------------------------
+# 1) Different beta, different sigma (Separate regresions)
+m_f = function(x) {
+  m <- list(4)
+  m[[1]] = lm(ave.add ~ I(yearc) + I((yearc)^2), x) # quad 
+  m[[2]] = lm(log(ave.add) ~ I(yearc) + I((yearc)^2), x) # lquad
+  m[[3]] = lm(ave.add ~ I(yearc), x) # lin
+  m[[4]] = lm(log(ave.add) ~ I(yearc), x) # llin
+  names(m)  <- c('quad', 'lquad', 'lin', 'llin')
+  
+  ldply(m, function(ms) {
+    data.frame(parameter=c(attributes(coef(ms))$names,"sigma"),
+               estimate=c(coef(ms), summary(ms)$sigma),
+               ci.sig = c( apply(confint(ms),1,prod) > 0 ,NA),
+               aic=AIC(ms), var=c(diag(vcov(ms)),NA))
+  }
+  )
+}
+mod.dd = ddply(bird.yeartotal, .(forestN,abbrev), m_f)
+colnames(mod.dd)[3] <- 'model'
+levels(mod.dd$parameter) <- c('b0', 'b2', 'b1', 'sigma')
+#--------------------------------------------------------
+
+#--------------------------------------------------------
+#2) Different betas, same sigma (one big regression)
+m_f2 = function(x) {
+  m <- list(4)
+  m[[1]] = lm(ave.add ~ I(yearc)*abbrev + I((yearc)^2)*abbrev, x) # quad 
+  m[[2]] = lm(log(ave.add) ~ I(yearc)*abbrev + I((yearc)^2)*abbrev, x) # lquad
+  m[[3]] = lm(ave.add ~ I(yearc)*abbrev, x) # lin
+  m[[4]] = lm(log(ave.add) ~ I(yearc)*abbrev, x) # llin
+  names(m)  <- c('quad', 'lquad', 'lin', 'llin')
+  
+  ldply(m, function(ms) {
+    data.frame(parameter=c(attributes(coef(ms))$names,"sigma"),
+               estimate=c(coef(ms), summary(ms)$sigma),
+               var=c(diag(vcov(ms)),NA))
+  }
+  )
+}
+
+mod.ds = ddply(bird.yeartotal, .(forestN), m_f2)
+
+colnames(mod.dd)[2] <- 'model'
+#--------------------------------------------------------
+
+
+#--------------------------------------------------------
+# 3) Hirerachical Beta, Same sigma (lmer random regression)
+
+# model using lmer, adding random effects. 
+mrnd_f <- function(x) {
+  x$lave <- log(x$ave.add) 
+  m <- list(2)
+  m[[1]] <-lmer(lave ~ yearc +(yearc|abbrev),data=x)
+  m[[2]] <-lmer(lave ~ yearc+I(yearc^2)+(yearc+I(yearc^2)|abbrev),data=x)  
+  names(m)  <- c('lin', 'quad')
+  
+  ldply(m, function(ms) {
+    aux1 <- triu( attributes(VarCorr(ms)$abbrev)$correlation,1 ) 
+    cor.val <- as.numeric(aux1[aux1!=0])  
+    cor.name <- c('rho_01','rho_02','rho_12')[1:length(cor.val)]
+    
+    rn <- ranef(ms, postVar=T)$abbrev   
+    colnames(rn) <- c('b0', 'b1', 'b2')[1:ncol(rn)]
+    rn1 <- melt(data.frame(rn, abbrev=rownames(rn)), id.vars='abbrev')
+    rn1$nam <- apply(rn1[,1:2],1, paste, collapse='.') 
+    v <- melt(t(apply(attributes(rn)$postVar, 3, diag)))
+    
+    data.frame(parameter=c(names(fixef(ms)),"residual",cor.name,rn1$nam),
+               mean=c(fixef(ms),0, cor.val, rn1$value),
+               variance=c(attributes(VarCorr(ms)$abbrev)$stddev,
+                          attributes(VarCorr(ms))$sc,rep(0,length(cor.val)),
+                          v$value) )
+  }
+  )
+}
+mod.hs <- ddply(bird.yeartotal, .(forestN), mrnd_f)
+#--------------------------------------------------------
+
+# Bayesian models
+# n is total of years within each species (17) 
+# ns is the total of species (73)
+
+#--------------------------------------------------------
+# 4)  DIfferents Betas, Hirerarchical Sigma 
+mtext.dh.quad = "
+model {
+for (i in 1:n) { 
+y[i] ~ dnorm(eff[i]+slop[i]+quad[i] , eta.e[abbrev[i]]  )
+eff[i]  <-  beta[1, abbrev[i]]
+slop[i] <-  beta[2, abbrev[i]]*year[i]
+quad[i] <-  beta[3, abbrev[i]]*year[i]^2
+}
+
+# Priors.  
+for (j in 1:ns) {
+for (i in 1:3) { beta[i,j]   ~ dunif(-1000, 1000) }
+eta.e[j]      ~ dgamma(alpha,lambda)
+sigma.e[j] <- 1/sqrt(eta.e[j])      
+}
+
+# hyperpriors
+alpha  ~ dunif(0,100)
+lambda ~ dunif(0,100)
+
+# predictives 
+# for (i in 1:ns) { 
+# y14[i] ~ dnorm(e[i]+s[i]+q[i] , eta.e[abbrev[i]]  )
+# e[i]  <-  beta[1, abbrev[i]]
+# s[i] <-  beta[2, abbrev[i]]*14
+# q[i] <-  beta[3, abbrev[i]]*14^2
+# rate[i] <- y14[i]/y13[i] - 1 
+# }
+}
+"
+mtext.dh.lin = "
+model {
+for (i in 1:n) { 
+y[i] ~ dnorm(eff[i]+slop[i] , eta.e[abbrev[i]]  )
+eff[i]  <-  beta[1, abbrev[i]]
+slop[i] <-  beta[2, abbrev[i]]*year[i]
+}
+
+# Priors.  
+for (j in 1:ns) {
+for (i in 1:2) { beta[i,j]   ~ dunif(-1000, 1000) }
+eta.e[j]      ~ dgamma(alpha,lambda)
+sigma.e[j] <- 1/sqrt(eta.e[j])      
+}
+
+# hyperpriors
+alpha  ~ dunif(0,100)
+lambda ~ dunif(0,100)
+}
+"
+#--------------------------------------------------------
+
+#--------------------------------------------------------
+#5) Hirerachical Betas, Differents Sigma
+mtext.hd.lin = "
+model {
+for (i in 1:n) { 
+y[i] ~ dnorm(eff[i]+slop[i] , eta.e[abbrev[i]]  )
+eff[i]  <-  beta[1, abbrev[i]]
+slop[i] <-  beta[2, abbrev[i]]*year[i]
+}
+
+# Priors.  
+for (j in 1:ns) {
+beta[1:2,j]  ~ dmnorm(mu,prec.be )
+sigma.e[j]   ~ dunif(0,1000)       
+eta.e[j]     <- 1/sqrt(sigma.e[j])
+}
+
+# hyperpriors
+prec.be   ~ dwish(R, df)
+sigma.be  <- inverse(prec.be)
+for (i in 1:2) { mu[i] ~ dnorm(0,0.001) }
+
+df     <- 3  
+alpha  ~ dunif(0,100)
+lambda ~ dunif(0,100)
+}
+"
+
+mtext.hd.quad = "
+model {
+for (i in 1:n) { 
+y[i] ~ dnorm(eff[i]+slop[i]+quad[i] , eta.e[abbrev[i]]  )
+eff[i]  <-  beta[1, abbrev[i]]
+slop[i] <-  beta[2, abbrev[i]]*year[i]
+quad[i] <-  beta[3, abbrev[i]]*year[i]^2
+}
+
+# Priors.  
+for (j in 1:ns) {
+beta[1:3,j]  ~ dmnorm(mu,prec.be )
+sigma.e[j]   ~ dunif(0,1000)       
+eta.e[j]     <- 1/sqrt(sigma.e[j])
+}
+
+# hyperpriors
+prec.be   ~ dwish(R, df)
+sigma.be  <- inverse(prec.be)
+for (i in 1:3) { mu[i] ~ dnorm(0,0.001) }
+
+df     <- 4  
+alpha  ~ dunif(0,100)
+lambda ~ dunif(0,100)
+}
+"
+#--------------------------------------------------------
+
+
+#--------------------------------------------------------
+# 6) Hirerarchical Sigma, Hirerarchical Beta
+mtext.hh.quad = "
+model {
+for (i in 1:n) { 
+y[i] ~ dnorm(eff[i]+slop[i]+quad[i] , eta.e[abbrev[i]]  )
+eff[i]  <-  beta[1, abbrev[i]]
+slop[i] <-  beta[2, abbrev[i]]*year[i]
+quad[i] <-  beta[3, abbrev[i]]*year[i]^2
+}
+
+# Priors.  
+for (j in 1:ns) {
+beta[1:3,j]   ~ dmnorm(mu,prec.be )
+eta.e[j]      ~ dgamma(alpha,lambda)
+sigma.e[j] <- 1/sqrt(eta.e[j])      
+}
+
+# hyperpriors
+prec.be   ~ dwish(R, df)
+sigma.be  <- inverse(prec.be)
+for (i in 1:3) { mu[i] ~ dnorm(0,0.001) }
+
+df     <- 4  
+alpha  ~ dunif(0,100)
+lambda ~ dunif(0,100)
+
+# predictives 
+eta.epred ~ dgamma(alpha,lambda)
+sigma.epred <- 1/sqrt(eta.epred)
+
+# for (i in 1:ns) { 
+# y14[i] ~ dnorm(e[i]+s[i]+q[i] , eta.e[abbrev[i]]  )
+# e[i]  <-  beta[1, abbrev[i]]
+# s[i] <-  beta[2, abbrev[i]]*14
+# q[i] <-  beta[3, abbrev[i]]*14^2
+# rate[i] <- y14[i]/y13[i] - 1 
+# }
+
+}
+"
+mtext.hh.lin = "
+model {
+for (i in 1:n) { 
+y[i] ~ dnorm(eff[i]+slop[i] , eta.e[abbrev[i]]  )
+eff[i]  <-  beta[1, abbrev[i]]
+slop[i] <-  beta[2, abbrev[i]]*year[i]
+}
+
+# Priors.  
+for (j in 1:ns) {
+beta[1:2,j]   ~ dmnorm(mu,prec.be )
+eta.e[j]      ~ dgamma(alpha,lambda)
+sigma.e[j] <- 1/sqrt(eta.e[j])      
+}
+
+# hyperpriors
+prec.be   ~ dwish(R, df)
+sigma.be  <- inverse(prec.be)
+for (i in 1:2) { mu[i] ~ dnorm(0,0.001) }
+
+df     <- 4  
+alpha  ~ dunif(0,100)
+lambda ~ dunif(0,100)
+}
+"
+#---------------------------------------------------------------
+
+# Run all jags models: 4,5,6 
+
+runjags <- function(d, model, l) {
+  # d <- subset(bird.yeartotal, forest=='9020')
+  dat = list(y = log(d$ave.add)  , 
+             abbrev = as.numeric(d$abbrev) ,
+             year= d$yearc, 
+             n = nrow(d), 
+             ns = nlevels(d$abbrev),
+             R = diag(l))
+  m = jags.model(textConnection(model), dat, n.chains=3, n.adapt=100)
+  update(m, 1000)
+  coda.samples(m, c('alpha','lambda','mu',"sigma.be",'beta'), 3000)
+}
+
+res.dh.q   <- dlply(bird.yeartotal, .(forestN), .fun=runjags, model=mtext.dh.quad, l=3)
+res.dh.lin <- dlply(bird.yeartotal, .(forestN), .fun=runjags, model=mtext.dh.lin, l=2)
+
+res.hd.q   <- dlply(bird.yeartotal, .(forestN), .fun=runjags, model=mtext.hd.quad, l=3)
+res.hd.lin <- dlply(bird.yeartotal, .(forestN), .fun=runjags, model=mtext.hd.lin, l=2)
+
+res.hh.q   <- dlply(bird.yeartotal, .(forestN), .fun=runjags, model=mtext.hh.quad, l=3)
+res.hh.lin <- dlply(bird.yeartotal, .(forestN), .fun=runjags, model=mtext.hh.lin, l=2)
+
+
+
+
+
+
+
+
+
+
+
+
--- R/yearlymodels.R
+++ R/yearlymodels.R
@@ -51,195 +51,53 @@ qplot(data=totales,x=year,xlab='Year',ylab='Bird Count',y=count,shape=forestN,co
 dev.off()
 #--------------
 
-# 1) Separate regresions: lm without random terms one per specie*forest
+# 2) Run 6 'cells' of models, calling file codemodels.R to run them and save results. 
 
-m_f = function(x) {
-  m <- list(4)
-  m[[1]] = lm(ave.add ~ I(yearc) + I((yearc)^2), x) # quad 
-  m[[2]] = lm(log(ave.add) ~ I(yearc) + I((yearc)^2), x) # lquad
-  m[[3]] = lm(ave.add ~ I(yearc), x) # lin
-  m[[4]] = lm(log(ave.add) ~ I(yearc), x) # llin
-  names(m)  <- c('quad', 'lquad', 'lin', 'llin')
-  
-  ldply(m, function(ms) {
-    data.frame(parameter=c(attributes(coef(ms))$names,"sigma"),
-             estimate=c(coef(ms), summary(ms)$sigma),
-             ci.sig = c( apply(confint(ms),1,prod) > 0 ,NA),
-             aic=AIC(ms), var=c(diag(vcov(ms)),NA))
-  }
-  )
-}
-models.lm = ddply(bird.yeartotal, .(forestN,abbrev), m_f)
-colnames(models.lm)[3] <- 'model'
-levels(models.lm$parameter) <- c('b0', 'b2', 'b1', 'sigma')
+# mod.dd:     dif beta, dif sigma, using lm()
+# mod.ds:     dif beta, same sigma, using lm() 
+# mod.hs:     hier beta, same sigma, using lmer()
+# res.dh.lin: dif beta, hier sigma, lin model, using jags 
+# res.dh.q:   dif beta, hier sigma, quad model, using jags
+# res.hd.lin: hier beta, dif sigma, lin model, using jags
+# res.hd.q:   hier beta, dif sigma, quad model, using jags
+# res.hh.lin: hier beta, hier sigma, lin model, using jags
+# res.hh.q:   hier beta, hier sigma, quad model, using jags    
 
+source('R\\codemodels.R')
+save(mod.dd, mod.ds, mod.hs, res.dh.lin, 
+     res.dh.q, res.hd.lin,res.hd.q, res.hh.lin,res.hh.q, 
+     file='modresults.Rdata')
+#===============================================================
+# 3 ) Summary of models, plots and tables 
 
-qplot(data=subset(models.lm, parameter=='b1'), x=model, y=aic,color=ci.sig, facets=~forestN)
-with(models.lm, table(parameter, ci.sig,model))
-tab1 <- ddply(models.lm, .(forestN,model,parameter),function(x) quantile(x$estimate, probs=c(0.25, 0.5, 0.975)) ) 
+# Separate regresions: lm without random terms one per specie*forest
+qplot(data=subset(mod.dd, parameter=='b1'), x=model, y=aic,color=ci.sig, facets=~forestN)
+with(mod.dd, table(parameter, ci.sig,model))
+tab1 <- ddply(mod.dd, .(forestN,model,parameter),function(x) quantile(x$estimate, probs=c(0.25, 0.5, 0.975)) ) 
 tab1 <- xtable(tab1, digits=3, caption='Percentiles for Separate Regresion model coefficients')
 print(tab1, file="summ_modlm.tex", include.rownames=FALSE)
 
 postscript('figs/hist_m1.ps')
 #qplot(data=models.lm, x=estimate,y=..density..,geom='histogram')+facet_wrap(facets=forestN~parameter,scale='free')
-qplot(data=subset(models.lm, parameter=='b1'), x=estimate,y=..density..,geom='histogram') +facet_wrap(facets=forestN~model,scale='free')
-
+qplot(data=subset(mod.dd, parameter=='b1'), x=estimate,y=..density..,geom='histogram') +facet_wrap(facets=forestN~model,scale='free')
 dev.off()
 
-aux <- reshape(models.lm , direction='wide', timevar='parameter', idvar=c('forestN','abbrev') )
+aux <- reshape(mod.dd , direction='wide', timevar='parameter', idvar=c('forestN','abbrev') )
 postscript('figs/scat_m1.ps')
 ggpairs(data=aux, columns=3:6, color='forestN')
 dev.off()
 
-#=====================================================
-# 2) Include random terms in the model
-
-# model using lmer, adding random effects. 
-mrnd_f <- function(x) {
-  x$lave <- log(x$ave.add) 
-  m <- list(2)
-  m[[1]] <-lmer(lave ~ yearc +(yearc|abbrev),data=x)
-  m[[2]] <-lmer(lave ~ yearc+I(yearc^2)+(yearc+I(yearc^2)|abbrev),data=x)  
-  names(m)  <- c('lin', 'quad')
-  
-  ldply(m, function(ms) {
-    aux1 <- triu( attributes(VarCorr(ms)$abbrev)$correlation,1 ) 
-    cor.val <- as.numeric(aux1[aux1!=0])  
-    cor.name <- c('rho_01','rho_02','rho_12')[1:length(cor.val)]
-    
-    rn <- ranef(ms, postVar=T)$abbrev   
-    colnames(rn) <- c('b0', 'b1', 'b2')[1:ncol(rn)]
-    rn1 <- melt(data.frame(rn, abbrev=rownames(rn)), id.vars='abbrev')
-    rn1$nam <- apply(rn1[,1:2],1, paste, collapse='.') 
-    v <- melt(t(apply(attributes(rn)$postVar, 3, diag)))
-    
-    data.frame(parameter=c(names(fixef(ms)),"residual",cor.name,rn1$nam),
-             mean=c(fixef(ms),0, cor.val, rn1$value),
-             variance=c(attributes(VarCorr(ms)$abbrev)$stddev,
-                        attributes(VarCorr(ms))$sc,rep(0,length(cor.val)),
-                        v$value) )
-  }
-  )
-}
-
-#x <- subset(bird.yeartotal, forest==9020) 
-#qplot(data=x, year,lave)
-#qplot(data=x[x$abbrev=='ALFL',], year,lave) + geom_line()
-
-models.rnd = ddply(bird.yeartotal, .(forestN), mrnd_f)
-
-ci_fun <- function(x,al) {
-    x <- as.numeric(x)
-    ci <- qnorm(c(al/2, 1-al/2), mean=x[1], sd=x[2]^2)
-    prod(ci) > 0
-}
-models.rnd$ci.sig<-  apply(models.rnd[, 4:5], 1, ci_fun, al=.05)
 levels(models.rnd$parameter)[c(1:2,4,5)] <- c('b0','sigma', 'b1', 'b2')
-
 tab2 <- xtable(models.rnd, digits=3, caption='Random coefficent regresion results')
 print(tab2, file="summ_modrnd.tex", include.rownames=FALSE)
 
 
-#=====================================================
-# 3) Bayes model
-
-# bayesian models: one model for each forest
-# using wishart distribution for coviariance matrix of beta coef 
-
-# n is total of years within each species (17) 
-# ns is the total of species (73)
-
-model.wishart = "
-model {
-for (i in 1:n) { 
-y[i] ~ dnorm(eff[i]+slop[i]+quad[i] , eta.e[abbrev[i]]  )
-eff[i]  <-  beta[1, abbrev[i]]
-slop[i] <-  beta[2, abbrev[i]]*year[i]
-quad[i] <-  beta[3, abbrev[i]]*year[i]^2
-}
-
-# Priors.  
-for (j in 1:ns) {
-  beta[1:3,j]   ~ dmnorm(mu,prec.be )
-  eta.e[j]      ~ dgamma(alpha,lambda)
-  sigma.e[j] <- 1/sqrt(eta.e[j])      
-}
-
-# hyperpriors
-prec.be   ~ dwish(R, df)
-sigma.be  <- inverse(prec.be)
-for (i in 1:3) { mu[i] ~ dnorm(0,0.001) }
-
-df     <- 4  
-alpha  ~ dunif(0,100)
-lambda ~ dunif(0,100)
-
-# predictives 
-eta.epred ~ dgamma(alpha,lambda)
-sigma.epred <- 1/sqrt(eta.epred)
-
-# for (i in 1:ns) { 
-# y14[i] ~ dnorm(e[i]+s[i]+q[i] , eta.e[abbrev[i]]  )
-# e[i]  <-  beta[1, abbrev[i]]
-# s[i] <-  beta[2, abbrev[i]]*14
-# q[i] <-  beta[3, abbrev[i]]*14^2
-# rate[i] <- y14[i]/y13[i] - 1 
-# }
-
-}
-"
-model.wishart.lin = "
-model {
-for (i in 1:n) { 
-y[i] ~ dnorm(eff[i]+slop[i] , eta.e[abbrev[i]]  )
-eff[i]  <-  beta[1, abbrev[i]]
-slop[i] <-  beta[2, abbrev[i]]*year[i]
-}
-
-# Priors.  
-for (j in 1:ns) {
-beta[1:2,j]   ~ dmnorm(mu,prec.be )
-eta.e[j]      ~ dgamma(alpha,lambda)
-sigma.e[j] <- 1/sqrt(eta.e[j])      
-}
-
-# hyperpriors
-prec.be   ~ dwish(R, df)
-sigma.be  <- inverse(prec.be)
-for (i in 1:2) { mu[i] ~ dnorm(0,0.001) }
-
-df     <- 4  
-alpha  ~ dunif(0,100)
-lambda ~ dunif(0,100)
-
-# predictives 
-eta.epred ~ dgamma(alpha,lambda)
-sigma.epred <- 1/sqrt(eta.epred)
-}
-"
-
-runjags <- function(d, model, l) {
-  # d <- subset(bird.yeartotal, forest=='9020')
-  dat = list(y = log(d$ave.add)  , 
-           abbrev = as.numeric(d$abbrev) ,
-           year= d$yearc, 
-           n = nrow(d), 
-           ns = nlevels(d$abbrev),
-           R = diag(l))
-  m = jags.model(textConnection(model), dat, n.chains=3, n.adapt=1000)
-  update(m, 1000)
-  coda.samples(m, c('alpha','lambda','mu',"sigma.be",'beta'), 3000)
-}
-
-res.wishart <- dlply(bird.yeartotal, .(forestN), .fun=runjags, model=model.wishart, l=3)
-res.wishart.lin <- dlply(bird.yeartotal, .(forestN), .fun=runjags, model=model.wishart.lin, l=2)
+# Bayesian models diag, models 4,5,6
 
 par.names <- attributes(res.wishart$Chippewa[[1]])$dimnames[[2]]
 par.names.lin <- attributes(res.wishart.lin$Chippewa[[1]])$dimnames[[2]]
-
 # gelman diagnostic, only 6 param from the covariance matrix  
 gquad <- llply(res.wishart, .fun=function(x) gelman.diag(x[, par.names[c(1,221:227,229:230,233)] ]) )
-
 llply(res.wishart, .fun=function(x) gelman.diag(x[, par.names.lin[c(1,148:152,154)] ]) )
 
 #plot(res.wishart[[1]][, par.names[1:3]])
@@ -279,6 +137,8 @@ t <- tab3[tab3$par %in% c('mu.1.','mu.2.','mu.3.','sigma.be.1.1.','sigma.be.2.2.
 t <- xtable(t, digits=3, caption='Mean and variances for Wishar Prior model')
 print(t, file="summ_wishart.tex", include.rownames=FALSE)
 #========================================================
+
+
 # collect results for bayesian model
 summfun <- function(res) {
   s <- summary(res)
