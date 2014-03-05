397553dd4d40bdda3c546218975c679c7ce11199
nachalca <ialvarez@iastate.edu>
2014-03-05 12:39:31 -0600

    add code for scaled data results with IW

--- R/reduced.R
+++ R/reduced.R
@@ -4,20 +4,21 @@ library(reshape2)
 library(rstan)
 set_cppo(mode = "fast")
 
-d <- list.files('../data', 'sims_')
-for ( i in 1:length(d) ) load(paste('../data/', d[i],sep=''))
+# d <- list.files('../data', 'sims_')
+# for ( i in 1:length(d) ) load(paste('../data/', d[i],sep=''))
+# 
+# x <- rbind(res_size10d2[[1]],res_size50d2[[1]],res_size250d2[[1]], res_size10d10[[1]],res_size50d10[[1]])
+# reduced.res  <- data.frame(dim=c(rep(2,1500*3),rep(60,600*2)),subset(x, param %in% c('mu[1]', 'mu[2]', 's1', 's2', 'rho') ) )
+# write.table(reduced.res, file='../data/reduced_res.csv', row.names=FALSE)
+# 
+# 
+# # checking times ..
+# ms=c('iw', 'siw', 'ss', 'ht')
+# time <- rbind(res_size10d2[[2]],res_size50d2[[2]],res_size250d2[[2]],res_size10d10[[2]],res_size50d10[[2]])
+# colnames(time) <- ms
+# df <- data.frame(n=c(10,50,250,10,50), dim=c(2,2,2,10,10), time)
+# write.csv(df, file='../data/timetable.csv', row.names=FALSE)
 
-x <- rbind(res_size10d2[[1]],res_size50d2[[1]],res_size250d2[[1]], res_size10d10[[1]],res_size50d10[[1]])
-reduced.res  <- data.frame(dim=c(rep(2,1500*3),rep(60,600*2)),subset(x, param %in% c('mu[1]', 'mu[2]', 's1', 's2', 'rho') ) )
-write.table(reduced.res, file='../data/reduced_res.csv', row.names=FALSE)
-
-
-# checking times ..
-ms=c('iw', 'siw', 'ss', 'ht')
-time <- rbind(res_size10d2[[2]],res_size50d2[[2]],res_size250d2[[2]],res_size10d10[[2]],res_size50d10[[2]])
-colnames(time) <- ms
-df <- data.frame(n=c(10,50,250,10,50), dim=c(2,2,2,10,10), time)
-write.csv(df, file='../data/timetable.csv', row.names=FALSE)
 
 # Run the IW model with scaled data on worst scenario
 
@@ -26,8 +27,8 @@ load('../data/simdata.Rdata')
 prms <- c('mu', 's1', 's2', 'rho')
 
 runstan.sim <- function(d, it = 1200, ch = 3, w=200, prm=NULL) {
-  K <- ncol(d[,-c(1:5)])
-  dat = list(y = d[,-c(1:5)] , N = nrow(d), R = diag(K), k=K)
+  K <- ncol(d[,-c(1:4)])
+  dat = list(y = d[,-c(1:4)] , N = nrow(d), R = diag(K), k=K)
   sampling(object=m_iw, data = dat,pars=prm, iter = it, chains = ch, warmup=w)
 }
 printresult <- function(xx) {
