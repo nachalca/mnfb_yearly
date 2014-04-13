230d7f10c15ae15c9bdcec612ab9a53e80e0f27b
nachalca <ialvarez@iastate.edu>
2014-04-13 10:44:48 -0500

    new version to get results

--- R/reduced.R
+++ R/reduced.R
@@ -4,31 +4,31 @@ library(reshape2)
 library(rstan)
 set_cppo(mode = "fast")
  
-# d <- list.files('../data', 'sims_')
-# for ( i in 1:length(d) ) load(paste('../data/', d[i],sep=''))
-# 
-# #x <- rbind(res_size10d2[[1]],res_size50d2[[1]],res_size250d2[[1]], res_size10d10[[1]],res_size50d10[[1]])
-# x <- rbind(res_size10d2[[1]],res_size50d2[[1]],res_size250d2_backup[[1]], res_size10d10[[1]],res_size50d10[[1]])
-# 
-# n1 <- dim(res_size10d2[[1]])[1]
-# n2 <- dim(res_size10d10[[1]])[1]
-# 
-# reduced.res  <- data.frame(dim=c(rep(2,n1*3),rep(10,n2*2)),x)
-# write.table(reduced.res, file='../data/reduced_res.csv', row.names=FALSE)
-# 
-# # get the number of iterations
-# diagres <- rbind(res_size10d2$diag,res_size50d2$diag,res_size250d2_backup$diag, res_size10d10$diag,res_size50d10$diag)
-# nn1 <- dim(res_size10d2$diag)[1] 
-# nn2 <- dim(res_size10d10$diag)[1]
-# diagres <- data.frame(dim=c(rep(2,nn1*3),rep(10,nn2*2)),diagres)
-# write.table(diagres, file='../data/diag_res.csv', row.names=FALSE)
-# 
-# # get the times
-# ms <- c('iw', 'siw', 'ss', 'ht')
-# times <- rbind(res_size10d2[[2]],res_size50d2[[2]],res_size250d2_backup[[2]],res_size10d10[[2]],res_size50d10[[2]])
-# colnames(times) <- ms
-# df <- data.frame(n=c(10,50,250,10,50), dim=c(2,2,2,10,10), times)
-# write.csv(df, file='../data/timetable.csv', row.names=FALSE)
+d <- list.files('../data', 'sims_')
+for ( i in 1:length(d) ) load(paste('../data/', d[i],sep=''))
+
+#x <- rbind(res_size10d2[[1]],res_size50d2[[1]],res_size250d2[[1]], res_size10d10[[1]],res_size50d10[[1]])
+x <- rbind(res_size10d2[[1]],res_size50d2[[1]],res_size250d2_backup[[1]], res_size10d10[[1]],res_size50d10[[1]])
+
+n1 <- dim(res_size10d2[[1]])[1]
+n2 <- dim(res_size10d10[[1]])[1]
+
+reduced.res  <- data.frame(dim=c(rep(2,n1*3),rep(10,n2*2)),x)
+write.table(reduced.res, file='../data/reduced_res.csv', row.names=FALSE)
+
+# get the number of iterations
+diagres <- rbind(res_size10d2$diag,res_size50d2$diag,res_size250d2_backup$diag, res_size10d10$diag,res_size50d10$diag)
+nn1 <- dim(res_size10d2$diag)[1] 
+nn2 <- dim(res_size10d10$diag)[1]
+diagres <- data.frame(dim=c(rep(2,nn1*3),rep(10,nn2*2)),diagres)
+write.table(diagres, file='../data/diag_res.csv', row.names=FALSE)
+
+# get the times
+ms <- c('iw', 'siw', 'ss', 'ht')
+times <- rbind(res_size10d2[[2]],res_size50d2[[2]],res_size250d2_backup[[2]],res_size10d10[[2]],res_size50d10[[2]])
+colnames(times) <- ms
+df <- data.frame(n=c(10,50,250,10,50), dim=c(2,2,2,10,10), times)
+write.csv(df, file='../data/timetable.csv', row.names=FALSE)
 
 
 
@@ -62,8 +62,8 @@ momd10  <- ddply(simd10, .(s,sim,r,ns), function(xx) c(apply(xx[,-c(1:4)],2,sd),
 colnames(momd10)[-c(1:4)] <- paste(rep(c('mean','sd'),each=10), rep(1:10,2), sep='.') 
 
 # scale each data set
-simd2  <- ddply(simd2, .(s,sim,r,ns) , function(xx) scale(xx[,-c(1:4)], center=FALSE))
-simd10 <- ddply(simd10, .(s,sim,r,ns) , function(xx) scale(xx[,-c(1:4)], center=FALSE))
+simd2  <- ddply(simdata.2, .(s,sim,r,ns) , function(xx) scale(xx[,-c(1:4)], center=FALSE))
+simd10 <- ddply(simdata.10, .(s,sim,r,ns) , function(xx) scale(xx[,-c(1:4)], center=FALSE))
 prms <- c('s1','s2','rho')
 
 mod_sciwd2  <-  dlply(simd2, .(s,sim,r,ns), runstan.sim, prm=prms, .parallel=parallel)
